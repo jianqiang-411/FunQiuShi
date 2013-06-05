e4b032f55a43d7494a679b40d0d2eed1447f8db6
lakkey <lakkey@lakkeytekiMac.local>
2013-06-05 09:59:55 +0800

    Initial Commit

---
+++ FunQiuShi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FunQiuShi
+//
+//  Created by lakkey on 13-6-5.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
